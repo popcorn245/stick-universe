cdc33c48c955e9b0e1e04098a3d583d5b71a25f5
Bobby Johnson <bobby@madnesslabs.net>
2021-11-30 23:51:57 -0600

    Add function and console logs

--- scripts.js
+++ scripts.js
@@ -22,11 +22,30 @@
 
 // NEW CODE FROM DAY 006
 
-function addTodo() {
+//GLOBAL
+const test = "something";
+let temp = 1;
+
+function addTodo(todoText = "test") {
+  // ISOLATED SCOPE
+  console.log(document);
   const todoEl = document.querySelector("#todo");
   const value = todoEl.value;
   const todoListEl = document.querySelector("#todo-list");
   const newItemEl = document.createElement("li");
-  newItemEl.textContent = value;
+  newItemEl.textContent = todoText;
   todoListEl.appendChild(newItemEl);
+  test = "wee";
+  // SIDE EFFECT (IMMUTABLE)
+  console.log(test);
 }
+
+console.log(test);
+
+function sendMessage(message) {
+  alert(message);
+}
+
+sendMessage("Hello Me");
+sendMessage("Say whatever");
+sendMessage("Hello Me");
